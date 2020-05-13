0baf2dd80e388b3375e05cd60b79702f3d58a34a
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-13 22:28:46 +0530

    feat : Added icon in each item of list

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {   // Add from this line ...
   final List<WordPair> _suggestions = <WordPair>[];
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  final Set<WordPair> _saved = Set<WordPair>();   // Add this line.
+  final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -47,11 +48,16 @@ class RandomWordsState extends State<RandomWords> {   // Add from this line ...
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(   // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),                // ... to here.
     );
   }
 
