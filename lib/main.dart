19a82c3508174e21ad1c16a7ce17338a094fe35f
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-13 21:10:52 +0530

    Added english word package which shows random words everytime

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          //child: Text('Hello World'),   // Replace this text...
+          child: Text(wordPair.asPascalCase),  // With this text.
         ),
       ),
     );
