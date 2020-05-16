cf1c55c08fbff49585523fa2ea75d7190e4fc9e8
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-16 22:17:06 +0530

    feat : Add chatmessage list ui and display chatmessage on handleSubmitte

--- exercise_2/lib/main.dart
+++ exercise_2/lib/main.dart
@@ -20,11 +20,22 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = []; // NEW
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   void _handleSubmitted(String text) {
-    print(text);
     _textController.clear();
+    ChatMessage message = ChatMessage(
+      //NEW
+      text: text, //NEW
+    ); //NEW
+    setState(() {
+      //NEW
+      _messages.insert(0, message); //NEW
+      _focusNode.requestFocus();  // NEW
+    }); //NEW
+
   }
 
   Widget _buildTextComposer() {
@@ -41,7 +52,9 @@ class _ChatScreenState extends State<ChatScreen> {
               child: TextField(
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
-                decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Send a message'),
+                focusNode: _focusNode,  // NEW
               ),
             ), // NEW
             Container(
@@ -58,8 +71,56 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return Column(                                        // MODIFIED
+        children: [                                        // NEW
+          Flexible(                                        // NEW
+            child: ListView.builder(                       // NEW
+              padding: EdgeInsets.all(8.0),                // NEW
+              reverse: true,                               // NEW
+              itemBuilder: (_, int index) => _messages[index], // NEW
+              itemCount: _messages.length,                 // NEW
+            ),                                             // NEW
+          ),                                               // NEW
+          Divider(height: 1.0),                            // NEW
+          Container(                                       // NEW
+            decoration: BoxDecoration(
+                color: Theme.of(context).cardColor),         // NEW
+            child: _buildTextComposer(),                   //MODIFIED
+          ),                                               // NEW
+        ],                                                 // NEW
+      );                                                // NE
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text}); // NEW
+  final String text;
+
+  String _name = 'Mansi';
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: _buildTextComposer(),
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
