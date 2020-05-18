8b59859def1628edf7bb57ec1ded672f7463b8b2
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-18 19:42:05 +0530

    feat: Add array of questions

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -26,9 +26,11 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
-  List<Icon> scoreKeeper = [
-    Icon(Icons.check,color: Colors.green,),
-    Icon(Icons.close,color: Colors.red,)
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
 
   @override
