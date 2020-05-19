356bc7b6dc8baa500e4400b953dd8fbe61b67345
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-19 21:20:51 +0530

    feat: Add getchoice1 and getchoice2 method to display two button's text

--- destini_flutter/lib/story_brain.dart
+++ destini_flutter/lib/story_brain.dart
@@ -7,7 +7,7 @@ List<Story> _storyData = [
       storyTitle:
       'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
       choice1: 'I\'ll hop in. Thanks for the help!',
-      choice2: 'Better ask him if he\'s a murderer first.'),
+      choice2: 'Better ask him if he\'s a murderer first.'),//called constructor of story class.
   Story(
       storyTitle: 'He nods slowly, unphased by the question.',
       choice1: 'At least he\'s honest. I\'ll climb in.',
@@ -39,16 +39,19 @@ String getStory(){
   return _storyData[0].storyTitle;
 }
 
+String getChoice1(){
+  return _storyData[0].choice1;
+}
+
+String getChoice2(){
+  return _storyData[0].choice2;
+}
+
 
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
-
-
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
-
-//TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
