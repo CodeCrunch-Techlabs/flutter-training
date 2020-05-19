a1c416dd04ed5a7b0d3ba7e456a68cb0292b6602
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-19 11:03:02 +0530

    doc: Add Quiz_brain.dart file with class QuizBrain

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -43,12 +43,6 @@ class _QuizPageState extends State<QuizPage> {
 
  // Question q1 = Question(q: 'You can lead a cow down stairs but not up stairs.', a: false );
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false ),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true ),
-    Question(q: 'A slug\'s blood is green..', a: true ),
-  ];
-
   List<Icon> scoreKeeper = [];
 
     int questionNumber = 0;
