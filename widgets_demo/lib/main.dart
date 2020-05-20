b81607f10757b315f3249cb2ad885748e3e8d716
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-20 17:16:00 +0530

    feat: Add PageView widget

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Widgets Demo"),
         ),
-        body: HomePage(),
+        body: PageViewPage(),
       )
     );
   }
@@ -32,35 +32,146 @@ class MyApp extends StatelessWidget {
 
 
 
-class HomePage extends StatefulWidget {
+// Check this link https://api.flutter.dev/flutter/widgets/PageController-class.html to get info about FutureBuilder widget.
+//https://www.youtube.com/watch?v=J1gE9xvph-A&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=10&pbjreload=10.
+
+class PageViewPage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _PageViewPageState createState() => _PageViewPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _PageViewPageState extends State<PageViewPage> {
 
-  // Check this link https://www.youtube.com/watch?v=2uaoEDOgk_I&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=9 to get info about FloatingActionButton widget.
+  final controller = PageController(
+    initialPage: 0,
+  );
 
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          backgroundColor: Colors.blue,
+      child : Scaffold(
+        body: PageView(
+          controller : controller,
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            HomePage(),
+            SecondPage(),
+            ThirdPage()
+          ],
         ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.yellow,
-          child: Container(
-            height: 50.0,
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       ),
     );
   }
 }
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.red,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.red,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.red,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.blue,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.blue,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.blue,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.green,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.green,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.green,
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );;
+  }
+}
+
+
 
 
 
