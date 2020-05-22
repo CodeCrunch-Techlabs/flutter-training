4d459d1b71603f609f99a99a5834d89b0ff183be
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-22 21:38:39 +0530

    feat: Add ReorderableListView widget

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -3,6 +3,9 @@ import 'dart:math' as math;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
+import 'package:fluttertoast/fluttertoast.dart';
+
+
 
 void main() => runApp(MyApp());
 
@@ -45,35 +48,135 @@ class Home extends StatelessWidget {
             ),
             backgroundColor: new Color(0xFF2979FF),
             centerTitle: true),
-        body: MyHomePage());
+        body: ReorderableListDemo());
   }
 }
 
-//check this https://www.youtube.com/watch?v=LPe56fezmoo&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=41.
+//check this https://medium.com/@adp4infotech4/flutter-building-a-reorderable-listview-735013719cf3.
 
-class MyHomePage extends StatefulWidget {
+
+class ReorderableListDemo extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ReorderableListDemo createState() => _ReorderableListDemo();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
+class ListViewCard extends StatefulWidget {
+  final int index;
+  final Key key;
+  final List<String> listItems;
+
+  ListViewCard(this.listItems, this.index, this.key);
+
+  @override
+  _ListViewCard createState() => _ListViewCard();
+}
+
+class _ListViewCard extends State<ListViewCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: SafeArea(
-          child: RichText(
-            text: TextSpan(
-              text: 'Hello ',
-              style: DefaultTextStyle.of(context).style,
-              children: <TextSpan>[
-                TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
-                TextSpan(text: ' world!'),
-                TextSpan(text: ' How are you ?'),
-              ],
+    return Card(
+      margin: EdgeInsets.all(4),
+      color: Colors.white,
+      child: InkWell(
+        splashColor: Colors.blue,
+        onTap: () => Fluttertoast.showToast(
+            msg: "Item ${widget.listItems[widget.index]} selected.",
+            toastLength: Toast.LENGTH_SHORT),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Title ${widget.listItems[widget.index]}',
+                      style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      textAlign: TextAlign.left,
+                      maxLines: 5,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Description ${widget.listItems[widget.index]}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 16),
+                      textAlign: TextAlign.left,
+                      maxLines: 5,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-         )
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: Icon(
+                Icons.reorder,
+                color: Colors.grey,
+                size: 24.0,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+class _ReorderableListDemo extends State<ReorderableListDemo> {
+  List<String> alphabetList = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reorderable ListView Demo"),
+      ),
+      body: ReorderableListView(
+        onReorder: _onReorder,
+        scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: List.generate(
+          alphabetList.length,
+              (index) {
+            return ListViewCard(
+              alphabetList,
+              index,
+              Key('$index'),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+
+
+
+  void _onReorder(int oldIndex, int newIndex) {
+    setState(
+          () {
+        final String item = alphabetList.removeAt(oldIndex);
+        alphabetList.insert(newIndex, item);
+      },
+    );
+  }
+}
+
