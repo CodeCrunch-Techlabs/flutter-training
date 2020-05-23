97199f572df99126f4b56d85a8d5b875172ee3a6
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-23 22:54:21 +0530

    feat: Implement animated position Widget

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -2,71 +2,128 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Widget _myAnimatedWidget = FirstWidget();
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              AnimatedSwitcher(
-                duration: const Duration(seconds: 1),
-                
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                    return ScaleTransition(child: child, scale: animation,);
-                  },
-      
-                child: _myAnimatedWidget,
+      title: 'AnimatedPositioned Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'AnimatedPositioned Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double pos_l = 0;
+  double pos_r = 0;
+  double pos_t = 0;
+  double pos_b = 0;
+
+  void _movewidget(String pos) {
+    setState(() {
+      if (pos == "Up") {
+        pos_l = 0;
+        pos_r = 0;
+        pos_t = 0;
+        pos_b = 100;
+      } else if (pos == "Right") {
+        pos_l = 100;
+        pos_r = 0;
+        pos_t = 0;
+        pos_b = 0;
+      } else if (pos == "Down") {
+        pos_l = 0;
+        pos_r = 0;
+        pos_t = 100;
+        pos_b = 0;
+      } else if (pos == "Left") {
+        pos_l = 0;
+        pos_r = 100;
+        pos_t = 0;
+        pos_b = 0;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Container(
+        child: Stack(
+          children: <Widget>[
+            AnimatedPositioned(
+              left: pos_l,
+              right: pos_r,
+              top: pos_t,
+              bottom: pos_b,
+              duration: Duration(milliseconds: 500),
+              child: Center(
+                child: Container(
+                  color: Colors.red,
+                  width: 100.0,
+                  height: 100.0,
+                ),
               ),
-              RaisedButton(
-                child: Text('Click here'),
+            ),
+            Positioned(
+              bottom: 100,
+              left: 160,
+              child: RaisedButton(
                 onPressed: () {
-                  setState(() {
-                    _myAnimatedWidget =
-                        (_myAnimatedWidget.toString() == "FirstWidget")
-                            ? SecondWidget()
-                            : FirstWidget();
-                  });
+                  _movewidget("Up");
                 },
-              )
-            ]),
-      ),
-    );
-  }
-}
-
-class FirstWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 200.0,
-      height: 200.0,
-      child: Center(child: Text("This is First Widget")),
-      decoration: BoxDecoration(
-        color: Colors.orange,
-      ),
-    );
-  }
-}
-
-class SecondWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 200.0,
-      height: 200.0,
-      child: Center(child: Text("This is Second Widget")),
-      decoration: BoxDecoration(
-        color: Colors.green,
-      ),
+                child: Icon(Icons.keyboard_arrow_up),
+              ),
+            ),
+            Positioned(
+              bottom: 60,
+              left: 260,
+              child: RaisedButton(
+                onPressed: () {
+                  _movewidget("Right");
+                },
+                child: Icon(Icons.keyboard_arrow_right),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              left: 160,
+              child: RaisedButton(
+                onPressed: () {
+                  _movewidget("Down");
+                },
+                child: Icon(Icons.keyboard_arrow_down),
+              ),
+            ),
+            Positioned(
+              bottom: 60,
+              left: 60,
+              child: RaisedButton(
+                onPressed: () {
+                  _movewidget("Left");
+                },
+                child: Icon(Icons.keyboard_arrow_left),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
