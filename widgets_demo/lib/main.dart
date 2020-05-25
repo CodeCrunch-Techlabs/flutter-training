e1dae4efdafdc7baec33a89ddcd2333b0d2bc38b
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-25 15:52:38 +0530

    feat: Add IgnorePointer widget

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
 
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -22,43 +21,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              height: 100,
-              width: 100,
-            ),
-            Divider(
-              color: Colors.black,
-              indent: 100,
-              endIndent: 100,
-              thickness: 10,
-              height: 50,
-            ),
-            Container(
-              color: Colors.green,
-              height: 100,
-              width: 100,
-            ),
-             Divider(
-               color: Colors.black,
-               indent: 100,
-               endIndent: 100,
-               thickness: 10,
-               height: 50,
-             ),
-            Container(
-              color: Colors.pink,
-              height: 100,
-              width: 100,
-            )
-          ],
-        )
-      )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widgets'),
+      ),
+      body: Center(
+        child: IgnorePointer(
+          child: IconButton(
+            icon: Icon(Icons.delete,
+            size: 50,
+            color: Colors.red,),
+            onPressed: (){
+              print("pressed");
+            },
+          ),
+          ignoring: true,
+        ),
+      ),
     );
   }
 }
