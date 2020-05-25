6c23ee560d356aa26784bc59bf183132d48f553f
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-25 23:36:24 +0530

    feat: Implement percentage indicator Widget

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-
+import 'package:percent_indicator/percent_indicator.dart';
 void main() => runApp(ClipPathApp());
 
 class ClipPathApp extends StatelessWidget {
@@ -31,29 +31,98 @@ class _ClipPathWidgetState extends State {
 
  @override
  Widget build(BuildContext context) {
+   
   return Scaffold(
    appBar: AppBar(title: Text("Sample ClipPath Widget"), backgroundColor: Colors.black45, ),
    body: Center(
-    child: Stack(
-     children: [
-      ClipPath( 
-       child: Container(
-        color: Colors.black,
-       ),
-
-      ),
-      Container(
-       child: Column(
-        children: [
-         Image.network("https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png", width: 100.0, height: 100.0, ),
-         Text("John Doe", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold), )
-        ],
-       ),
-      )
-
-     ],
-    ),
-   )
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: 140.0,
+                lineHeight: 14.0,
+                percent: 0.5,
+                center: Text(
+                  "50.0%",
+                  style: new TextStyle(fontSize: 12.0),
+                ),
+                trailing: Icon(Icons.mood),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                backgroundColor: Colors.grey,
+                progressColor: Colors.blue,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: 170.0,
+                animation: true,
+                animationDuration: 1000,
+                lineHeight: 20.0,
+                leading: new Text("left content"),
+                trailing: new Text("right content"),
+                percent: 0.2,
+                center: Text("20.0%"),
+                linearStrokeCap: LinearStrokeCap.butt,
+                progressColor: Colors.red,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 50,
+                animation: true,
+                lineHeight: 20.0,
+                animationDuration: 2000,
+                percent: 0.9,
+                center: Text("90.0%"),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                progressColor: Colors.greenAccent,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 50,
+                animation: true,
+                lineHeight: 20.0,
+                animationDuration: 2500,
+                percent: 0.8,
+                center: Text("80.0%"),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                progressColor: Colors.green,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                children: <Widget>[
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.2,
+                    progressColor: Colors.red,
+                  ),
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.5,
+                    progressColor: Colors.orange,
+                  ),
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.9,
+                    progressColor: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+   ),
   );
  }
 }
