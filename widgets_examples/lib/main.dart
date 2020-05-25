79e29ab1808825839e30b5fe09a7aaae8c2c7794
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-25 11:08:18 +0530

    feat: Implement IndexedStack Widget

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -1,58 +1,46 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(AnimatedContainerApp());
+void main() => runApp(MyApp());
 
-class AnimatedContainerApp extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _AnimatedContainerAppState createState() => _AnimatedContainerAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
-  double _width = 50;
-  double _height = 50;
-  Color _color = Colors.green;
-  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+class _MyAppState extends State<MyApp> {
+  int i = 2;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "IndexedStack Example",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('AnimatedContainer Demo'),
-        ),
-        body: Center(
-          child: AnimatedContainer(
-            width: _width,
-            height: _height,
-            decoration: BoxDecoration(
-              color: _color,
-              borderRadius: _borderRadius,
+        appBar: AppBar(title: Text("IndexedStack Example")),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: IndexedStack(
+                index: i,
+                children: <Widget>[
+                  Container(color: Colors.red,),
+                  Container(color: Colors.green),
+                  Container(color: Colors.blue),
+                ],
+              ),
             ),
-            duration: Duration(seconds: 1),
-            curve: Curves.fastOutSlowIn,
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.play_arrow),
-          onPressed: () {
-            setState(() {
-              final random = Random();
-
-              _width = random.nextInt(300).toDouble();
-              _height = random.nextInt(300).toDouble();
-
-              _color = Color.fromRGBO(
-                random.nextInt(256),
-                random.nextInt(256),
-                random.nextInt(256),
-                1,
-              );
-              _borderRadius =
-                  BorderRadius.circular(random.nextInt(100).toDouble());
-            });
-          },
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  if(i < 2){
+                     i++; 
+                  } else{
+                    i = 0;
+                  }
+                });
+              },
+              child: Text('Show Next Widget'),
+            )
+          ],
         ),
       ),
     );
