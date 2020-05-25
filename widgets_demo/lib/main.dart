56d0d3c43664f691b2278a10aa49cfd4c27a3b74
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-25 15:36:34 +0530

    feat: Add Divider widget

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -23,12 +23,41 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.white,
       child: Container(
-        child: Center(
-          child: LinearProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-          )
-        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              height: 100,
+              width: 100,
+            ),
+            Divider(
+              color: Colors.black,
+              indent: 100,
+              endIndent: 100,
+              thickness: 10,
+              height: 50,
+            ),
+            Container(
+              color: Colors.green,
+              height: 100,
+              width: 100,
+            ),
+             Divider(
+               color: Colors.black,
+               indent: 100,
+               endIndent: 100,
+               thickness: 10,
+               height: 50,
+             ),
+            Container(
+              color: Colors.pink,
+              height: 100,
+              width: 100,
+            )
+          ],
+        )
       )
     );
   }
