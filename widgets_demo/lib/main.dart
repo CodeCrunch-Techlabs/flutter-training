e9a3e38e28eea5174ecae48b45fa25fc0a675e99
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-25 16:32:23 +0530

    feat: Add CupertinoActivityIndicator widget

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -19,25 +20,70 @@ class MyHomePage extends StatefulWidget {
 
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  Future<String> _calculation = Future<String>.delayed(
+    Duration(seconds: 3),
+        () => 'Data Loaded',
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Widgets'),
+    return Container(
+      color: Colors.white,
+        child: Center(
+          child: FutureBuilder<String>(
+          future: _calculation, // a previously-obtained Future<String> or null
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+      List<Widget> children;
+      if (snapshot.hasData) {
+          children = <Widget>[
+            Icon(
+              Icons.check_circle_outline,
+              color: Colors.green,
+              size: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Result: ${snapshot.data}'),
+            )
+          ];
+      } else if (snapshot.hasError) {
+          children = <Widget>[
+            Icon(
+              Icons.error_outline,
+              color: Colors.red,
+              size: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Error: ${snapshot.error}'),
+            )
+          ];
+      } else {
+          children = <Widget>[
+            SizedBox(
+              child: CupertinoActivityIndicator(
+                radius: 40,
+              ),
+              width: 60,
+              height: 60,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text('Awaiting result...'),
+            )
+          ];
+      }
+      return Center(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: children,
       ),
-      body: Center(
-        child: IgnorePointer(
-          child: IconButton(
-            icon: Icon(Icons.delete,
-            size: 50,
-            color: Colors.red,),
-            onPressed: (){
-              print("pressed");
-            },
+      );
+          },
           ),
-          ignoring: false,
         ),
-      ),
     );
   }
 }
