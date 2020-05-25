74ea28a61ef69a66f1469a6fcb1815306f77da49
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-25 16:55:18 +0530

    feat: Implement slider Widget

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -1,97 +1,60 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final europeanCountries = [
-      'Albania',
-      'Andorra',
-      'Armenia',
-      'Austria',
-      'Azerbaijan',
-      'Belarus',
-      'Belgium',
-      'Bosnia and Herzegovina',
-      'Bulgaria',
-      'Croatia',
-      'Cyprus',
-      'Czech Republic',
-      'Denmark',
-      'Estonia',
-      'Finland',
-      'France',
-      'Georgia',
-      'Germany',
-      'Greece',
-      'Hungary',
-      'Iceland',
-      'Ireland',
-      'Italy',
-      'Kazakhstan',
-      'Kosovo',
-      'Latvia',
-      'Liechtenstein',
-      'Lithuania',
-      'Luxembourg',
-      'Macedonia',
-      'Malta',
-      'Moldova',
-      'Monaco',
-      'Montenegro',
-      'Netherlands',
-      'Norway',
-      'Poland',
-      'Portugal',
-      'Romania',
-      'Russia',
-      'San Marino',
-      'Serbia',
-      'Slovakia',
-      'Slovenia',
-      'Spain',
-      'Sweden',
-      'Switzerland',
-      'Turkey',
-      'Ukraine',
-      'United Kingdom',
-      'Vatican City'
-    ];
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ListViews',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: Scaffold(
-          appBar: AppBar(title: Text('ListViews')),
-          body: ListView.builder(
-            itemCount: europeanCountries.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: SelectableText(
-                  europeanCountries[index],
-                  showCursor: true,
-                  cursorWidth: 5,
-                  cursorColor: Colors.green,
-                  cursorRadius: Radius.circular(0),
-                ),
-              );
-            },
-          )),
-    );
+ import 'package:flutter/material.dart';
+  
+  void main() => runApp(SliderExample());
+  
+  class SliderExample extends StatefulWidget {
+    @override
+    _SliderExampleState createState() {
+      return _SliderExampleState();
+    }
   }
-}
-
-class BodyLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return _myListView(context);
+  
+  class _SliderExampleState extends State {
+    int _value = 6;
+  
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Slider Tutorial',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Slider Tutorial'),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(
+                    Icons.ac_unit,
+                    size: 30,
+                  ),
+                  new Expanded(
+                    child: Slider(
+                        value: _value.toDouble(),
+                        min: 1.0,
+                        max: 10.0,
+                        divisions: 10,
+                        activeColor: Colors.red,
+                        inactiveColor: Colors.black,
+                        label: 'Set a value',
+                        onChanged: (double newValue) {
+                          setState(() {
+                            _value = newValue.round();
+                          });
+                        },
+                        semanticFormatterCallback: (double newValue) {
+                          return '${newValue.round()} dollars';
+                        }
+                    )
+                  ),
+                ]
+              )
+            ),
+          )
+        ),
+      );
+    }
   }
-}
-
-// replace this function with the code in the examples
-Widget _myListView(BuildContext context) {
-  return ListView();
-}
