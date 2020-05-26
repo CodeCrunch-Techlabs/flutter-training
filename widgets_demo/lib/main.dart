93a2f078f41a0e7b171f12325121086ebbcae0a1
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-26 21:39:58 +0530

    feat: Add ClipPath widget

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -1,52 +1,66 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as Math;
-
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Smiley Face',
-      home: Scaffold(
-          body: Container(
-            constraints: BoxConstraints.expand(),
-            child: Smiley(),
-          )),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF02BB9F),
+        primaryColorDark: const Color(0xFF167F67),
+        accentColor: const Color(0xFF02BB9F),
+      ),
+      home: MyHomePage(title: 'Flutter Clip Path'),
     );
   }
 }
-
-class Smiley extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: SmileyPainter(),
+
+    var screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ClipPath'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: ClipPath(
+          child: Container(
+            width: screenSize.width,
+            height: 200,
+            color: Colors.blue,
+          ),
+          clipper: CustomClipPath(),
+        ),
+      ),
     );
   }
 }
 
-class SmileyPainter extends CustomPainter {
+class CustomClipPath extends CustomClipper<Path> {
+  var radius=10.0;
   @override
-  void paint(Canvas canvas, Size size) {
-    final radius = Math.min(size.width, size.height) / 2;
-    final center = Offset(size.width / 2, size.height / 2);
-    // Draw the body
-    final paint = Paint()..color = Colors.yellow;
-    canvas.drawCircle(center, radius, paint);
-    // Draw the mouth
-    final smilePaint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 20;
-    canvas.drawArc(Rect.fromCircle(center: center, radius: radius / 2), 0,
-        Math.pi, false, smilePaint);
-    // Draw the eyes
-    canvas.drawCircle(
-        Offset(center.dx - radius / 2, center.dy - radius / 2), 20, Paint());
-    canvas.drawCircle(
-        Offset(center.dx + radius / 2, center.dy - radius / 2), 20, Paint());
-  }
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 100);
+    path.quadraticBezierTo(
+        size.width / 2, size.height,
+        size.width, size.height - 100);
+    path.lineTo(size.width, 0);
+    path.close();
 
+    return path;
+  }
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
