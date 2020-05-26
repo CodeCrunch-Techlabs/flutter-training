d8ead0b5f38995c1944dbd93c5ce5808bf38a0ef
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-26 12:38:28 +0530

    feat: Implement checkbox list view widgets

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -1,49 +1,92 @@
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(DividerApp());
-
-class DividerApp extends StatelessWidget {
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.product}) : super(key: key);
+  final List<Product> product;
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sample Divider Widget',
-      home: DividerWidget(),
-    );
+  _ShoppingListState createState() {
+    return new _ShoppingListState();
   }
 }
 
-class DividerWidget extends StatefulWidget {
-  DividerWidget({Key key}) : super(key: key);
-
-  @override
-  _DividerWidgetState createState() => _DividerWidgetState();
-}
-
-class _DividerWidgetState extends State<DividerWidget> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class _ShoppingListState extends State<ShoppingList> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sample Divider'),
-        backgroundColor: Colors.black87, 
-      ),
-      body: ClipRRect(
-        borderRadius: BorderRadius.circular(15.0),
-        child: Container(
-          child: Align(
-            alignment: Alignment.topLeft,
-            widthFactor: 0.75,
-            heightFactor: 0.75,
-            child: Image.network('https://www.woolha.com/media/2019/08/flutter-cliprect.jpg'),
-          ),
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Shopping"),
         ),
-      ),
-    );
+        body: new Container(
+          padding: new EdgeInsets.all(8.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              new Expanded(
+                  child: new ListView(
+                padding: new EdgeInsets.symmetric(vertical: 8.0),
+                children: widget.product.map((Product product) {
+                  return new ShoppingItemList(product);
+                }).toList(),
+              )),
+              new RaisedButton(
+                onPressed: () {
+                  for (Product p in widget.product) {
+                    if (p.isCheck) print(p.name);
+                  }
+                },
+                child: new Text('Buy'),
+              )
+            ],
+          ),
+        ));
   }
+}
+
+class ShoppingItemList extends StatefulWidget {
+  final Product product;
+  ShoppingItemList(Product product)
+      : product = product,
+        super(key: new ObjectKey(product));
+  @override
+  ShoppingItemState createState() {
+    return new ShoppingItemState(product);
+  }
+}
+
+class ShoppingItemState extends State<ShoppingItemList> {
+  final Product product;
+  ShoppingItemState(this.product);
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+        title: new Row(
+      children: <Widget>[
+        new Expanded(child: new Text(product.name)),
+        new Checkbox(
+            value: product.isCheck,
+            onChanged: (bool value) {
+              setState(() {
+                product.isCheck = value;
+              });
+            })
+      ],
+    ));
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'ListView CheckBox',
+    home: ShoppingList(product: [
+      new Product('Eggs', false),
+      new Product('Flour', false),
+      new Product('Chocolate chips', false)
+    ]),
+  ));
+}
+
+class Product {
+  String name;
+  bool isCheck;
+  Product(this.name, this.isCheck);
 }
