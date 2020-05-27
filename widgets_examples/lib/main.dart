49b3f6935f0d0bbfe52bf8960c5f28206d50668e
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-27 11:52:12 +0530

    feat:backdrop filter

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -1,89 +1,69 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
+        title: 'BackDrop filter',
+        theme: ThemeData( 
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Home(),
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
 
-class _MyHomePageState extends State<MyHomePage> {
-
-  Future<String> _calculation = Future<String>.delayed(
-    Duration(seconds: 3),
-        () => 'Data Loaded',
-  );
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-        child: Center(
-          child: FutureBuilder<String>(
-          future: _calculation, // a previously-obtained Future<String> or null
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-      List<Widget> children;
-      if (snapshot.hasData) {
-          children = <Widget>[
-            Icon(
-              Icons.check_circle_outline,
-              color: Colors.green,
-              size: 60,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text('Result: ${snapshot.data}'),
-            )
-          ];
-      } else if (snapshot.hasError) {
-          children = <Widget>[
-            Icon(
-              Icons.error_outline,
-              color: Colors.red,
-              size: 60,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text('Error: ${snapshot.error}'),
-            )
-          ];
-      } else {
-          children = <Widget>[
-            SizedBox(
-              child: CupertinoActivityIndicator(
-                radius: 40,
+    return Scaffold(
+        appBar: new AppBar(
+            elevation: 0.0,
+            title: new Text( "Backdrop Filter",
+              style: new TextStyle(
+                  color: Colors.white, 
+                  letterSpacing: 1.0
               ),
-              width: 60,
-              height: 60,
             ),
-            const Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: Text('Awaiting result...'),
-            )
-          ];
-      }
-      return Center(
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: children,
-      ),
-      );
-          },
+            backgroundColor: new Color(0xFF2979FF),
+            centerTitle: true
+        ),
+        body: LayoutPage()
+        );
+  }
+} 
+class LayoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Image.network('https://dummyimage.com/600x400/000/fff'),
+        Center(
+          child: ClipRect(  
+            child: BackdropFilter(
+              filter: ui.ImageFilter.blur(
+                sigmaX: 3.0,
+                sigmaY: 3.0,
+              ),
+              child: Container(
+                alignment: Alignment.center,
+                width: 200.0,
+                height: 200.0,
+                child: Text('Jaydip Patel',style: TextStyle(color: Colors.white),),
+              ),
+            ),
           ),
         ),
+      ],
     );
   }
 }
