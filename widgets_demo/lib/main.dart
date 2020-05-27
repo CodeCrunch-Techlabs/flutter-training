d7cbc3cc66b1a92e082f5a7ae8e0a46c376d6c47
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-27 22:39:05 +0530

    feat: Add DetailScreen class to show details page on onPressed event

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -40,15 +40,45 @@ class HomeScreen extends StatelessWidget {
                 middle: (i == 0) ? Text('Articels') : Text('Views'),
               ),
               child: Center(
-                child: Text(
-                  'This is tab #$i',
-                  style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+                child: CupertinoButton(
+                  child: Text(
+                    'This is tab #$i',
+                    style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+                  ),
+                  onPressed: (){
+                    Navigator.of(context).push(
+                      CupertinoPageRoute(builder:  (context) {
+                        return DetailScreen(i == 0 ? 'Articels' : 'Views');
+                      })
+                    );
+                  },
                 ),
               ),
             );
           },
         );
       },
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen(this.topic);
+
+  final String topic;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Details'),
+      ),
+      child: Center(
+        child: Text(
+          'Details for $topic',
+          style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+        ),
+      ),
     );
   }
 }
