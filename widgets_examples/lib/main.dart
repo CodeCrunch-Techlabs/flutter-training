4970934f6e6f9c82fb516ad31cd36de404475301
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-27 14:33:34 +0530

    feat: Semantics widgets

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:ui' as ui;
-
+ 
+ 
 
 void main() => runApp(MyApp());
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'BackDrop filter',
+        title: 'Sementic',
         theme: ThemeData( 
           primarySwatch: Colors.blue,
         ),
@@ -19,51 +19,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: new AppBar(
             elevation: 0.0,
-            title: new Text( "Backdrop Filter",
+            title: new Text(
+              "Flutter Sementics",
               style: new TextStyle(
                   color: Colors.white, 
-                  letterSpacing: 1.0
-              ),
+                  letterSpacing: 1.0),
             ),
             backgroundColor: new Color(0xFF2979FF),
-            centerTitle: true
-        ),
-        body: LayoutPage()
-        );
-  }
-} 
-class LayoutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return  Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Image.network('https://dummyimage.com/600x400/000/fff'),
-        Center(
-          child: ClipRect(  
-            child: BackdropFilter(
-              filter: ui.ImageFilter.blur(
-                sigmaX: 3.0,
-                sigmaY: 3.0,
-              ),
-              child: Container(
-                alignment: Alignment.center,
-                width: 200.0,
-                height: 200.0,
-                child: Text('Jaydip Patel',style: TextStyle(color: Colors.white),),
+            centerTitle: true),
+        body:SafeArea(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Semantics(
+                  child: Image.network(
+                      'https://dummyimage.com/600x400/000/fff'),
+                label: 'sementics image',
+                readOnly: true,
               ),
             ),
-          ),
+          ],
         ),
-      ],
-    );
+      ),
+    ),);
   }
 }
+ 
