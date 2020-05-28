e5cd12d0efb0e595e36351b8b50778198a5c9093
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-28 12:28:14 +0530

    feat: Implements two buttons

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -29,54 +29,87 @@ final String server =
 final List<Kitten> _kittens = <Kitten>[
   Kitten(
     name: "Mittens",
-    description: "The pinacle of cats. when Mittens sits in your lap, you feel like royalty.",
+    description:
+        "The pinacle of cats. when Mittens sits in your lap, you feel like royalty.",
     age: 11,
-    imageUrl:
-         "images/kitty1.jpg",
+    imageUrl: "images/kitty1.jpg",
   ),
   Kitten(
     name: "Fluffy",
     description: "World\'s cutest  kitten. Seriously. we dod the research.",
     age: 6,
-    imageUrl:
-        "images/kitty5.jpg",
+    imageUrl: "images/kitty5.jpg",
   ),
   Kitten(
     name: "Scooter",
     description: "Chases string faster then 9/10 competing kittens.",
     age: 2,
-    imageUrl:
-        "images/kitty2.jpg",
+    imageUrl: "images/kitty2.jpg",
   ),
   Kitten(
     name: "Steve",
     description: "Steave is cool and just kind of hangs out.",
     age: 4,
-    imageUrl:
-          "images/kitty3.jpg",
+    imageUrl: "images/kitty3.jpg",
   ),
   Kitten(
     name: "Flocky",
     description: "Flocky is cute.",
     age: 26,
-    imageUrl:
-        "images/kitty4.jpg",
+    imageUrl: "images/kitty4.jpg",
   ),
 ];
 
 class MyHomepage extends StatelessWidget {
   Widget _dialogBuilder(BuildContext context, Kitten kitten) {
+    ThemeData localTheme = Theme.of(context);
     return SimpleDialog(
-      children:[
+      contentPadding: EdgeInsets.zero,
+      children: [
         Image.asset(
           kitten.imageUrl,
           fit: BoxFit.fill,
         ),
-        Padding(  
+        Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(   
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-
+              Text(
+                kitten.name,
+                style: localTheme.textTheme.display1,
+              ),
+              Text(
+                '${kitten.age} months old',
+                style: localTheme.textTheme.subhead.copyWith(
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              Text(
+                kitten.description,
+                style: localTheme.textTheme.body1,
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Wrap(
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text("I\'M ALLERGIC"),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text("ADOPT"),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         )
