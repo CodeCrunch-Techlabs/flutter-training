9763e7ca1282688beb9cb8843784d90d6164a956
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-28 17:04:49 +0530

    feat: Display todolist

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -1,135 +1,44 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
+void main() {
+  runApp(MaterialApp(
+    title: 'Returning Data',
+    home: HomeScreen(),
+  ));
+}
 
 
-void main() => runApp(MyApp());
+class Todo {
+  final String title;
+  final String description;
 
+  Todo(this.title, this.description);
+}
 
-class MyApp extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
+
+  final todos = List<Todo>.generate(
+    20,
+        (i) => Todo(
+      'Todo $i',
+      'A description of what needs to be done for Todo $i',
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        onGenerateRoute: (settings) {
-          // If you push the PassArguments route
-          if (settings.name == ExtractArgumentsScreen.routeName) {
-            // Cast the arguments to the correct type: ScreenArguments.
-            final ScreenArguments args = settings.arguments;
-
-            // Then, extract the required data from the arguments and
-            // pass the data to the correct screen.
-            return MaterialPageRoute(
-              builder: (context) {
-                return ExtractArgumentsScreen(
-                  title: args.title,
-                  message: args.message,
-                );
-              },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Returning Data Demo'),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(todos[index].title),
             );
-          }
-          // The code only supports PassArgumentsScreen.routeName right now.
-          // Other values need to be implemented if we add them. The assertion
-          // here will help remind us of that higher up in the call stack, since
-          // this assertion would otherwise fire somewhere in the framework.
-          assert(false, 'Need to implement ${settings.name}');
-          return null;
-        },
-        title: 'Navigation with Arguments',
-        home: HomePage(),
-        routes: {
-          ExtractArgumentsScreen.routeName: (context) =>
-              ExtractArgumentsScreen(),
-        }
-    );
-  }
-}
-
-class ScreenArguments {
-  final String title;
-  final String message;
-
-  ScreenArguments(this.title, this.message);
-}
-
-
-class ExtractArgumentsScreen extends StatelessWidget {
-  static const routeName = '/extractArguments';
-
-  final String title;
-  final String message;
-
-  // This Widget accepts the arguments as constructor parameters. It does not
-  // extract the arguments from the ModalRoute.
-  //
-  // The arguments are extracted by the onGenerateRoute function provided to the
-  // MaterialApp widget.
-  const ExtractArgumentsScreen({
-    Key key,
-    @required this.title,
-    @required this.message,
-  }) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    // Extract the arguments from the current ModalRoute settings and cast
-    // them as ScreenArguments.
-    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(args.title),
-      ),
-      body: Center(
-        child: Text(args.message),
-      ),
-    );
-  }
-}
-
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("HomePage"),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text("Dog"),
-              onPressed: () {
-                // When the user taps the button, navigate to a named route
-                // and provide the arguments as an optional parameter.
-                Navigator.pushNamed(
-                  context,
-                  ExtractArgumentsScreen.routeName,
-                  arguments: ScreenArguments(
-                    'Dog',
-                    'This dog have a name puppy',
-                  ),
-                );
-              },
-            ),
-            RaisedButton(
-              child: Text("Cat"),
-              onPressed: () {
-                // When the user taps the button, navigate to a named route
-                // and provide the arguments as an optional parameter.
-                Navigator.pushNamed(
-                  context,
-                  ExtractArgumentsScreen.routeName,
-                  arguments: ScreenArguments(
-                    'Cat',
-                    'This Cat have a name mini',
-                  ),
-                );
-              },
-            ),
-          ],
+          },
         ),
       ),
     );
