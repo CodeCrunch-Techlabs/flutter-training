be444644e6bf6cdfaf2fd940759ec2e67be8c766
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-28 17:06:06 +0530

    feat: Add DetailScreen class to show details

--- widgets_demo/lib/main.dart
+++ widgets_demo/lib/main.dart
@@ -44,3 +44,25 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+class DetailScreen extends StatelessWidget {
+
+  final Todo todo;
+
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
+}
