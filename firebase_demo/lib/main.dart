cc5b06c33e12ba81729edff487f40da93d685ad1
Mansi Joshi <mansijoshi1777@gmail.com>
2020-05-29 22:49:21 +0530

    fix: Fix cloud_firestore error

--- firebase_demo/lib/main.dart
+++ firebase_demo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,17 +45,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document){
+       return ListTile(
+         title: Row(
+           children: <Widget>[
+             Expanded(
+               child: Text(document['name']),
+             ),
+             Container(
+               decoration: const BoxDecoration(
+                 color: Colors.red,
+               ),
+               padding: const EdgeInsets.all(10.0),
+               child: Text(document['votes'].toString()),
+             )
+           ],
+         ),
+         onTap: (){
+           print("Increase the votes");
+         },
+       );
   }
 
   @override
@@ -71,41 +83,18 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('brandname').snapshots(),
+        builder: (context, snapshot){
+          if(!snapshot.hasData) return const Text('Loading..');
+          return ListView.builder(
+            itemExtent: 80.0,
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) =>
+                     _buildListItem(context, snapshot.data.documents[index]),
+          );
+        }
+      )// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
