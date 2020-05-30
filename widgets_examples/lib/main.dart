69559697be14851e1b7cdb49e72238a785d7301e
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-30 20:09:44 +0530

    feat: Implement Animated Routing

--- widgets_examples/lib/main.dart
+++ widgets_examples/lib/main.dart
@@ -1,57 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(HeroApp());
-
-class HeroApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Transition Demo',
-      home: MainScreen(),
-    );
-  }
+main() {
+  runApp(MaterialApp(
+    home: Page1(),
+  ));
 }
 
-class MainScreen extends StatelessWidget {
-  @override
+class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Screen'),
-      ),
-      body: GestureDetector(
-        child: Hero(
-          tag: 'imageHero',
-          child: Image.network(
-            'https://picsum.photos/250?image=9',
-          ),
+      appBar: AppBar(),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go!'),
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
         ),
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return DetailScreen();
-          }));
-        },
       ),
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  @override
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+class Page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        child: Center(
-          child: Hero(
-            tag: 'imageHero',
-            child: Image.network(
-              'https://picsum.photos/250?image=9',
-            ),
-          ),
-        ),
-        onTap: () {
-          Navigator.pop(context);
-        },
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Page 2'),
       ),
     );
   }
