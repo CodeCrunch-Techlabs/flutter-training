b6ef54ebebdfd7b302b708ed7a9e20b5d060e0bf
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-31 18:11:51 +0530

    feat: Implement photolist class

--- json_demo/lib/main.dart
+++ json_demo/lib/main.dart
@@ -24,29 +24,69 @@ class Photo {
 }
  
 List<Photo> parsePhotos(String responseBody) {
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>(); 
   return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
 }
 
 Future<List<Photo>> fetchPhotos(http.Client client) async {
   final response =
-      await client.get('https://jsonplaceholder.typicode.com/photos');
-
+      await client.get('https://jsonplaceholder.typicode.com/photos'); 
   return parsePhotos(response.body);
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'JSON Demo';
-
+    final appTitle = 'JSON Demo'; 
     return MaterialApp(
       title: appTitle,
       home: MyHomePage(title: appTitle),
     );
   }
 }
+
+class MyHomePage extends StatelessWidget {
+  final String title; 
+  MyHomePage({Key key, this.title}) : super(key: key); 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+
+          return snapshot.hasData
+              ? PhotosList(photos: snapshot.data)
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
+
+class PhotosList extends StatelessWidget {
+  final List<Photo> photos;
+
+  PhotosList({Key key, this.photos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Image.network(photos[index].thumbnailUrl);
+      },
+    );
+  }
+}
+
 
 
  
