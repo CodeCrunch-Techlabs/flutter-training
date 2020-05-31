f88d77d399768eb8b47e60c202f51c42eae2876c
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-05-31 17:59:11 +0530

    refactor: Remove the photo file

--- json_demo/lib/main.dart
+++ json_demo/lib/main.dart
@@ -4,17 +4,20 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'JSON Demo',
-      theme: ThemeData( 
-        primarySwatch: Colors.blue, 
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home:,
+class Photo {
+  final int id;
+  final String title;
+  final String thumbnailUrl;
+
+  Photo({this.id, this.title, this.thumbnailUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
     );
   }
 }
+
  
