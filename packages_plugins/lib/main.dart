5467d4e630c549162e430e926af7f145772656b0
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-06-01 17:38:11 +0530

    feat: Add library css_colors

--- packages_plugins/lib/main.dart
+++ packages_plugins/lib/main.dart
@@ -1,16 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+ import 'package:css_colors/css_colors.dart';
+ import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+ void main() {
+   runApp(MyApp());
+ }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DemoPage(),
-    );
-  }
-}
- 
+ class MyApp extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home: DemoPage(),
+     );
+   }
+ }
+
+ class DemoPage extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(body: Container(color: CSSColors.orange));
+   }
+ }
