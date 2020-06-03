47f11102cf6e3c827956a95dd82319bb8f35be83
Mansi Joshi <mansijoshi1777@gmail.com>
2020-06-03 14:49:44 +0530

    feat: Add mapEventState and _mapStartTostate in timer_bloc.dart file

--- bloclibrary_demo/lib/bloc/timer_bloc.dart
+++ bloclibrary_demo/lib/bloc/timer_bloc.dart
@@ -1,9 +1,21 @@
+import 'dart:async';
+import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
-import 'package:flutter_timer/bloc/bloc.dart';
+import 'package:bloclibrarydemo/ticker.dart';
+import 'timer_event.dart';
+import 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
 
+  final Ticker _ticker;
+
   final int _duration = 60;
+
+  StreamSubscription<int> _tickerSubscription;
+
+  TimerBloc({@required Ticker ticker})
+      : assert(ticker != null),
+        _ticker = ticker;
 
   @override
   TimerState get initialState => // TODO: implement initialState;
@@ -12,6 +24,26 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
   Stream<TimerState> mapEventToState(
       TimerEvent event,
       ) async* {
-    // TODO: implement mapEventToState
+    if (event is Start) {
+      yield* _mapStartToState(event);
+    }
   }
+
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+
+  Stream<TimerState> _mapStartToState(Start start) async* {
+    yield Running(start.duration);
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: start.duration)
+        .listen((duration) => add(Tick(duration: duration)));
+  }
+
+
 }
