e2d061e9fb3001a16db7fe69db81ea7e06229240
Mansi Joshi <mansijoshi1777@gmail.com>
2020-06-03 14:16:12 +0530

    chore: Add ticker.dart with Ticker class

--- bloclibrary_demo/lib/main.dart
+++ bloclibrary_demo/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'counterbrain.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,58 +12,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider<CounterBloc>( // Allow child class to use state of BlocProvider class.
-        create: (context) => CounterBloc(),
-        child: CounterPage(), //This is child which going to use state of CounterBloc.
-      ),
+      home: TimerPage(), //This is child which going to use state of CounterBloc.
     );
   }
 }
 
-class CounterPage extends StatelessWidget {
+
+class TimerPage extends StatefulWidget {
+  @override
+  _TimerPageState createState() => _TimerPageState();
+}
+
+class _TimerPageState extends State<TimerPage> {
   @override
   Widget build(BuildContext context) {
-
-    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
-
-    return Scaffold(
-      appBar: AppBar(title: Text('Counter')),
-      body: BlocBuilder<CounterBloc, int>(
-        builder: (context, count) {
-          return Center(
-            child: Text(
-              '$count',
-              style: TextStyle(fontSize: 24.0),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: Icon(Icons.add),
-              onPressed: () {
-                //Increment the value
-                counterBloc.add(CounterEvent.increment);
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: Icon(Icons.remove),
-              onPressed: () {
-                //decrement the value
-                counterBloc.add(CounterEvent.decrement);
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
+
