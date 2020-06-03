ede7e9175527e1d99df6a2b757171c5ff1b7377b
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-06-03 16:58:28 +0530

    feat: Implement implement mapEventToState function

--- timer_app/lib/bloc/timer_bloc.dart
+++ timer_app/lib/bloc/timer_bloc.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
-import 'timer_state.dart';
 import 'timer_event.dart';
+import 'timer_state.dart';
+import 'package:timer_app/ticker.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  final Ticker _ticker;
   final int _duration = 60;
+
+  StreamSubscription<int> _tickerSubscription;
+
+  TimerBloc({@required Ticker ticker})
+      : assert(ticker != null),
+        _ticker = ticker;
 
   @override
   TimerState get initialState => Ready(_duration);
@@ -12,6 +22,22 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
   Stream<TimerState> mapEventToState(
     TimerEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is Start) {
+      yield* _mapStartToState(event);
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  Stream<TimerState> _mapStartToState(Start start) async* {
+     yield Running(start.duration);
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: start.duration)
+        .listen((duration) => add(Tick(duration: duration)));
   }
 }
