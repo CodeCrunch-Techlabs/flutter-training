598e13d705d64be8ec51b54a505fd9d98a9cce84
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-06-05 16:22:45 +0530

    chore: Add weatherModal.dat file and class

---
+++ weather_app/lib/weather_modal.dart
@@ -0,0 +1,14 @@
+class WeatherModal {
+  final temp;
+  final pressure;
+  final humidity;
+  final temp_max;
+  final temp_min;
+
+  double get getTemp => temp - 272.5;
+  double get getMaxTemp => temp_max - 272.5;
+  double get getMinTemp => temp_min - 272.5;
+
+  WeatherModal(
+      this.temp, this.pressure, this.humidity, this.temp_max, this.temp_min);
+}
