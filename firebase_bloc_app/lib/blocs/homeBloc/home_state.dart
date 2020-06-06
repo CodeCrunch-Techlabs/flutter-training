b85bbe9e6677e36cb0c3cefddd3647f4c6dd83ee
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-06-06 18:41:20 +0530

    feat: Implement Homepage State class

---
+++ firebase_bloc_app/lib/blocs/homeBloc/home_state.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+abstract class HomePageState extends Equatable {}
+
+class LogOutInitial extends HomePageState {
+  @override
+  
+  List<Object> get props => null;
+}
+
+class LogOutSuccess extends HomePageState {
+  @override
+  List<Object> get props => null;
+}
