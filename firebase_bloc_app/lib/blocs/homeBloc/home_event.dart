e9d926d3cd404749697a37ff5a9f740250e87828
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-06-06 18:34:18 +0530

    feat: Implement Homepage Event class

---
+++ firebase_bloc_app/lib/blocs/homeBloc/home_event.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+abstract class HomePageEvent extends Equatable {}
+
+class LogOutButtonPressedEvent extends HomePageEvent {
+  @override
+  List<Object> get props => null;
+}
