a9ca7526ef5d04922a66ceeffd9c9d67e31058a5
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-06-06 15:34:43 +0530

    feat: Implement firebase auth,signin,signout methods

--- firebase_bloc_app/lib/repository/user_repo.dart
+++ firebase_bloc_app/lib/repository/user_repo.dart
@@ -11,4 +11,23 @@ class UserRepo {
         email: email, password: password);
     return result.user;
   }
+
+  Future<FirebaseUser> signInUser(String email, String password) async {
+    var result = await firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return result.user;
+  }
+
+  Future<void> signOut() async {
+    await firebaseAuth.signOut();
+  }
+
+  Future<bool> isSignedIn() async {
+    var currentUser = await firebaseAuth.currentUser();
+    return currentUser != null;
+  }
+
+  Future<FirebaseUser> getCurrentUser() async {
+    return await firebaseAuth.currentUser();
+  }
 }
