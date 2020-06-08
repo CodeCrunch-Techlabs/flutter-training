97effd89996941aae7104d053e2f7d2a6875a382
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-06-08 18:32:15 +0530

    feat: Implement Homepage ui

--- firebase_bloc_app/lib/pages/homepage.dart
+++ firebase_bloc_app/lib/pages/homepage.dart
@@ -1,12 +1,33 @@
+import 'package:firebase_bloc_app/blocs/homeBloc/home_bloc.dart';
+import 'package:firebase_bloc_app/blocs/homeBloc/home_event.dart';
+import 'package:firebase_bloc_app/blocs/homeBloc/home_state.dart';
+import 'package:firebase_bloc_app/pages/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+class HomePageParent extends StatelessWidget {
+  FirebaseUser user;
+  HomePageParent({@required this.user});
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create:  (context)=>HomePageBloc(),
+      child: HomePage(user),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   FirebaseUser user;
+  HomePageBloc homePageBloc;
+
   HomePage(this.user);
 
   @override
   Widget build(BuildContext context) {
+    homePageBloc = BlocProvider.of<HomePageBloc>(context);
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
@@ -20,7 +41,11 @@ class HomePage extends StatelessWidget {
                 Icons.close,
                 color: Colors.white,
               ),
-              onPressed: () {},
+              onPressed: () {
+                homePageBloc.add(
+                  LogOutButtonPressedEvent(),
+                );
+              },
             ),
           ],
         ),
@@ -30,7 +55,23 @@ class HomePage extends StatelessWidget {
             Container(
               alignment: Alignment.center,
               child: Text(user.email),
-            )
+            ),
+            BlocListener<HomePageBloc, HomePageState>(
+              listener: (context, state) {
+                if (state is LogOutSuccess) {
+                  return navigateToSignUpPage(context);
+                }
+              },
+              child: BlocBuilder<HomePageBloc, HomePageState>(
+                builder: (context, state) {
+                  if (state is LogOutInitial) {
+                    return Container();
+                  } else if (state is LogOutSuccess) {
+                    return Container();
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -38,6 +79,8 @@ class HomePage extends StatelessWidget {
   }
 
   void navigateToSignUpPage(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {}));
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return SignUpPageParent();
+    }));
   }
 }
