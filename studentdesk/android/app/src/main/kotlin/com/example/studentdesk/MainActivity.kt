0cf57058d5703b1ad94bebdf5c72583e1f21d0fc
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-07-28 12:50:47 +0530

    chore: Create new project studentdesk

---
+++ studentdesk/android/app/src/main/kotlin/com/example/studentdesk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studentdesk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ studentdesk/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 14,
+        navigateAfterSeconds: new HomePage(),
+        title: new Text(
+          'Welcome To BookExchange App',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+        ),
+        // image: new Image.asset('images/splash.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(
+            color: Colors.amberAccent[200], fontWeight: FontWeight.bold),
+        photoSize: 100.0,
+        loaderColor: Colors.amberAccent[200]);
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Welcome"),
+      ),
+    );
+  }
+}
