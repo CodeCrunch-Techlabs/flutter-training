6089e51d2e2531829fb8e3687e1966c8e5a9c600
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-09-01 17:02:29 +0530

    feat: Add Transactions App

--- flutter_transaction_data/android/app/src/main/kotlin/com/example/flutter_transaction_data/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_transaction_data
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ transaction_app/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+import 'models/transaction.dart';
+import 'widgets/chart.dart';
+import 'widgets/new_trans.dart';
+import 'widgets/transaction_list.dart';
+ 
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Expence',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'OpenSans',
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String ammountInput;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    // Transaction(
+    //     id: 't1', title: 'New Shirt', ammount: 59.99, date: DateTime.now()),
+    // Transaction(
+    //     id: 't2', title: 'New Jeans', ammount: 33.99, date: DateTime.now()),
+    // Transaction(
+    //     id: 't1', title: 'New Shoes', ammount: 34.99, date: DateTime.now())
+  ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransaction.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
+  void _addNewTransactions(String txTitle, double txAmmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      ammount: txAmmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            child: NewTransaction(_addNewTransactions),
+            onTap: () {},
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Chart(_recentTransactions),
+            TransactionList(_userTransaction),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+    );
+  }
+}
