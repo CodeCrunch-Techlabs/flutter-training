b773c9ad89812ab2adae3ac2fce32bfba0f906ed
Mansi Joshi <mansijoshi1777@gmail.com>
2020-09-04 17:28:04 +0530

    feat: Add alert dialog for delete item from cart

--- shop_app/lib/Widget/Cart_Item.dart
+++ shop_app/lib/Widget/Cart_Item.dart
@@ -19,12 +19,33 @@ class CartItems extends StatelessWidget {
       key: ValueKey(id),
       background: Container(
         color: Theme.of(context).errorColor,
-        child: Icon(Icons.delete, color: Colors.white, size: 40,),
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
         alignment: Alignment.centerRight,
       ),
       direction: DismissDirection.endToStart,
-      onDismissed: (direction){
-         Provider.of<Cart>(context,listen: false).removeItem(productId);
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Are you sure?'),
+                  content:
+                      Text('Do you want to remove the item from the cart?'),
+               actions: <Widget>[
+                 FlatButton(child: Text('No'), onPressed: (){
+                   Navigator.of(context).pop(false);
+                 },),
+                 FlatButton(child: Text('Yes'), onPressed: (){
+                   Navigator.of(context).pop(true);
+                 },),
+               ],
+                ));
+      },
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
       child: Card(
         margin: EdgeInsets.symmetric(horizontal: 14, vertical: 4),
