161f0c758bbfd09f58ddfc2369fd44568a50b601
Mansi Joshi <mansijoshi1777@gmail.com>
2020-09-14 12:56:22 +0530

    feat: Add Slivers with FlexibleSpaceBar

--- shop_app/lib/Screens/Product_Details.dart
+++ shop_app/lib/Screens/Product_Details.dart
@@ -19,42 +19,46 @@ class ProductDetails extends StatelessWidget {
         productId); //listen: false means when evrytime products object is changed it will not load this evrytime.
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
-                tag: productId,
-                child: Image.network(
-                  product.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+//      appBar: AppBar(
+//        title: Text(product.title),
+//      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(title: Text(product.title),background: Hero(
+              tag: productId,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '\$${product.price}',
-              style: TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-                padding: EdgeInsets.all(4),
-                child: Text(
-              product.description,
-              textAlign: TextAlign.center,
-            ))
-          ],
-        ),
+            ),),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '\$${product.price}',
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(4),
+                  child: Text(
+                    product.description,
+                    textAlign: TextAlign.center,
+                  )),
+              SizedBox(height: 800,)
+            ]),
+          ),
+        ],
       ),
     );
   }
