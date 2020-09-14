2cc4ed63c883ced203560987232f3cf463206725
Mansi Joshi <mansijoshi1777@gmail.com>
2020-09-14 11:24:17 +0530

    feat: Add AnimatedContainer

--- shop_app/lib/Screens/auth_screen.dart
+++ shop_app/lib/Screens/auth_screen.dart
@@ -203,14 +203,15 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child:AnimatedBuilder(animation: _heightanimation,builder: (ctx, ch) => Container(
-//        height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightanimation.value.height,
-          constraints: BoxConstraints(minHeight: _heightanimation.value.height),
+      child:AnimatedContainer(
+        duration: Duration(milliseconds: 400),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+//          height: _heightanimation.value.height,
+          constraints: BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
           width: deviceSize.width * 0.75,
           padding: EdgeInsets.all(16.0),
-      child: ch,),
-        child: Form(
+      child:Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
