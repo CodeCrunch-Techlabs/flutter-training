a214417913f6438e6b5699b6a2e560da349c3414
Mansi Joshi <mansijoshi1777@gmail.com>
2020-09-16 22:36:10 +0530

    chore: Add flutter chat app with firebase connection

---
+++ flutterchat/lib/Screens/Chat_Screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(itemCount: 10, itemBuilder: (ctx, index) => Container(
+        padding: EdgeInsets.all(8),
+        child: Text('This works!'),
+      )),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),onPressed: (){
+        FirebaseFirestore.instance.collection('chats/42EknlarAYOL3ITvxIvH/messages').snapshots().listen((data) {
+          print(data.docs[0].i);
+        });
+      },),
+    );
+  }
+}
