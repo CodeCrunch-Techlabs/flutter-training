9d845c15a408d2e058297734f3b796552e752196
Mansi Joshi <mansijoshi1777@gmail.com>
2020-09-21 18:06:11 +0530

    feat: Add push notification

--- flutterchat/lib/Screens/Chat_Screen.dart
+++ flutterchat/lib/Screens/Chat_Screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:flutterchat/Widgets/Message.dart';
 import 'package:flutterchat/Widgets/New_Message.dart';
 
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+
+  @override
+  void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging();
+    fbm.configure(onMessage: (msg) {
+      print(msg);
+      return;
+    }, onLaunch: (msg){
+      print(msg);
+      return;
+    }, onResume: (msg){
+      print(msg);
+      return;
+    });
+    fbm.subscribeToTopic('chat');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
