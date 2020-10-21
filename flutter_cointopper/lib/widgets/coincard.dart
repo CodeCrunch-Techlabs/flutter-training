b9bb748dbc75f068f442264bbc48faa1616531e9
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-10-21 18:10:18 +0530

    feat: Adding card slider

---
+++ flutter_cointopper/lib/widgets/coincard.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class CoinCard extends StatefulWidget {
+  @override
+  _CoinCardState createState() => _CoinCardState();
+}
+
+class _CoinCardState extends State<CoinCard> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 10, right: 10, top: 15, bottom: 10),
+          width: MediaQuery.of(context).size.width * 1,
+          child: Text(
+            "Top Viewed",
+            style: TextStyle(
+              color: Colors.blue[800],
+              fontSize: 16,
+            ),
+          ),
+        ),
+        // Center(
+        //   child: SizedBox(
+        //     height: 100, // card height
+        //     child: PageView.builder(
+        //       itemCount: 10,
+        //       controller: PageController(viewportFraction: 0.5,initialPage: 1,),
+        //       onPageChanged: (int index) => setState(() => _index = index),
+        //       itemBuilder: (_, i) {
+        //         return Card(
+        //           elevation: 2,
+        //           shape: RoundedRectangleBorder(
+        //               borderRadius: BorderRadius.circular(20)),
+        //           child: Center(
+        //             child: Text(
+        //               "Card ${i+1}",
+        //               style: TextStyle(fontSize: 32),
+        //             ),
+        //           ),
+        //         );
+        //       },
+        //     ),
+        //   ),
+        // )
+
+        Container(
+          height: 110.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              _cardSlider(context, "Bitcoin", "9120.78", "4.46", "bitcoin.png",
+                  0xFFF0B028, 0xFFF0B028),
+              _cardSlider(context, "Ethereum", "230.91", "3.49", "eth.png",
+                  0xFF505050, 0xFF282828),
+              _cardSlider(context, "Litecoin", "62.19", "3.34", "ltc.png",
+                  0xFFDE4546, 0xFFE70814),
+              _cardSlider(context, "Ripple", "0.23978", "2.61", "ripple.png",
+                  0xFF689898, 0xFF08F880),
+              _cardSlider(context, "Tether", "1.00", "0.44", "tether.png",
+                  0xFFF86800, 0xFF404040),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Widget _cardSlider(BuildContext context, String coin, String rate, String price,
+    String imageUrl, int lcolor, int rcolor) {
+  return Card(
+    elevation: 3,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    child: Container(
+      padding: EdgeInsets.all(10),
+      width: MediaQuery.of(context).size.width * 0.5,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [Color(lcolor), Color(rcolor)],
+          ),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            coin,
+            style: TextStyle(
+              color: Colors.white60,
+            ),
+          ),
+          Text(
+            "\$$rate",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 26,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Image(
+                    width: 15,
+                    height: 15,
+                    image: AssetImage("assets/images/up_arrow.png"),
+                  ),
+                  SizedBox(
+                    width: 6,
+                  ),
+                  Text(
+                    "\$$price%",
+                    style: TextStyle(
+                      color: Colors.white60,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white30,
+                ),
+                child: Image(
+                  width: 25,
+                  height: 25,
+                  image: AssetImage("assets/images/$imageUrl"),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    ),
+  );
+}
--- flutter_cointopper/lib/widgets/dashboard.dart
+++ flutter_cointopper/lib/widgets/dashboard.dart
@@ -1,113 +1,129 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:flutter_cointopper/widgets/coincard.dart';
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 200,
-        padding: EdgeInsets.only(top: 30, left: 10, right: 10),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [Colors.blue[800], Colors.blue[400]],
-          ),
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        children: [
+          Container(
+            height: 180,
+            padding: EdgeInsets.only(top: 30, left: 10, right: 10),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.blue[800], Colors.blue[400]],
+              ),
+            ),
+            child: Column(
               children: [
-                Text(
-                  "Welcome to Cointopper",
-                  style: TextStyle(fontSize: 18, color: Colors.white60),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Crypto M.CAP",
-                        style: TextStyle(fontSize: 16, color: Colors.white60)),
-                    Text("\$207.00 B",
-                        style: TextStyle(fontSize: 20, color: Colors.white)),
+                    Text(
+                      "Welcome to Cointopper",
+                      style: TextStyle(fontSize: 18, color: Colors.white60),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text("Crypto M.CAP",
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.white60)),
+                        Text("\$207.00 B",
+                            style:
+                                TextStyle(fontSize: 20, color: Colors.white)),
+                      ],
+                    ),
                   ],
                 ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "Cryptocurrencies",
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+                Row(
+                  children: [
+                    Text(
+                      "Cryptocurrencies",
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 200,
-                  child: TextField(
-                    style: TextStyle(fontSize: 22.0, color: Colors.white60),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(12)),
-                      filled: true,
-                      hintText: 'Search',
-                      hintStyle: TextStyle(
-                        color: Colors.white60,
-                        fontSize: 20,
-                      ),
-                      contentPadding: EdgeInsets.only(
-                        left: 14.0,
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: TextField(
+                        style: TextStyle(fontSize: 22.0, color: Colors.white60),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(12)),
+                          filled: true,
+                          hintText: 'Search',
+                          hintStyle: TextStyle(
+                            color: Colors.white60,
+                            fontSize: 20,
+                          ),
+                          contentPadding: EdgeInsets.only(
+                            left: 14.0,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(13),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF00e00),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Text(
-                    'USD',
-                    style: TextStyle(fontSize: 18, color: Colors.white60),
-                  ),
-                ), 
-                Container(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF00e00),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.star_border,color: Colors.white60, ),
-                  ),
-                ),
-                 Container(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF00e00),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.notifications_active,color: Colors.white60,),
-                  ),
-                ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      padding: EdgeInsets.all(13),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00e00),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Text(
+                        'USD',
+                        style: TextStyle(fontSize: 18, color: Colors.white60),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00e00),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.star_border,
+                          color: Colors.white60,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00e00),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.notifications_active,
+                          color: Colors.white60,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+          CoinCard(),
+        ],
       ),
     );
   }
